f1df091caa7f5a34a637676172218d4f97e68bb1
Pranav Gupta <Pranav.19142@davietjal.org>
2022-05-25 22:58:28 +0530

    Home page created

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:daviet_app/custom_widgets/home_page_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -8,8 +7,8 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
           extendBodyBehindAppBar: true,
           drawer: Drawer(
             child: ListView(
@@ -29,9 +28,7 @@ class Homepage extends StatelessWidget {
             backgroundColor: Colors.transparent,
             shadowColor: Colors.transparent,
             elevation: 0,
-            centerTitle: true,
             title: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Image.asset(
                   "lib/images/college_logo-removebg-preview.png",
@@ -50,7 +47,8 @@ class Homepage extends StatelessWidget {
               SizedBox(
                 width: double.infinity,
                 height: 400,
-                child: Image.asset("lib/images/infra.jpg",
+                child: Image.asset(
+                  "lib/images/infra.jpg",
                   fit: BoxFit.cover,
                 ),
               ),
@@ -61,40 +59,114 @@ class Homepage extends StatelessWidget {
                     width: double.infinity,
                     height: 130,
                     decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
+                        gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
                           Colors.black,
                           Colors.black87,
                           Colors.black54,
-                        ]
-                      )
-                    ),
+                        ])),
                   ),
-                   Container(
-                     padding: const EdgeInsets.all(24.0),
-                     width: double.infinity,
+                  Container(
+                    padding: const EdgeInsets.all(32.0),
+                    width: double.infinity,
                     height: 450,
-                     decoration: const BoxDecoration(
-                         color: Color(0xff17181f),
-                       borderRadius: BorderRadius.only(topRight: Radius.circular(42),topLeft: Radius.circular(42))
-                     ),
-                     child: Column(
-                       children: [
-                         Row(
-                           children: const [
-                             HomeWidget(),
-                           ],
-                         )
-                       ],
-                     ),
+                    decoration: const BoxDecoration(
+                        color: Color(0xff17181f),
+                        //color: Color(0xff272848),
+                        //color: Colors.black,
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(42),
+                            topLeft: Radius.circular(42))),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            InkWell(
+                              child: const HomeWidget(
+                                image: "lib/images/attendance.png",
+                                title: "Attendance",
+                              ),
+                              onTap: (){},
+                            ),
+                            InkWell(
+                              child: const HomeWidget(
+                                image: "lib/images/notes.png",
+                                title: "Notes",
+                              ),
+                              onTap: (){},
+                            ),
+                            InkWell(
+                              child: const HomeWidget(
+                                image: "lib/images/placement.png",
+                                title: "Placements",
+                              ),
+                              onTap: (){},
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            InkWell(
+                              child:const HomeWidget(
+                                image: "lib/images/library.png",
+                                title: "Library",
+                              ),
+                              onTap: (){},
+                            ),
+                            InkWell(
+                              child:const HomeWidget(
+                                image: "lib/images/scholarship.png",
+                                title: "Scholarship",
+                              ),
+                              onTap: (){},
+                            ),
+                            InkWell(
+                              child:const HomeWidget(
+                                image: "lib/images/map.png",
+                                title: "Map",
+                              ),
+                              onTap: (){},
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            InkWell(
+                              child: const HomeWidget(
+                                image: "lib/images/hostel.png",
+                                title: "Hostel",
+                              ),
+                              onTap: (){},
+                            ),
+                            InkWell(
+                              child:const HomeWidget(
+                                image: "lib/images/announcement.png",
+                                title: "Announcement",
+                              ),
+                              onTap: (){},
+                            ),
+                            InkWell(
+                              child:const HomeWidget(
+                                image: "lib/images/clubs.png",
+                                title: "Clubs",
+                              ),
+                              onTap: (){},
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   )
                 ],
               )
             ],
-          )
-        ),
+          )),
     );
   }
 }
--- lib/custom_widgets/home_page_widgets.dart
+++ lib/custom_widgets/home_page_widgets.dart
@@ -1,14 +1,14 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 
 class HomeWidget extends StatelessWidget {
-  const HomeWidget({Key? key}) : super(key: key);
-
+  const HomeWidget({Key? key, required this.title, required this.image}) : super(key: key);
+  final String title;
+  final String image;
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 70,
-      width: 70,
+      padding: const EdgeInsets.all(4),
       decoration: const BoxDecoration(
         gradient: LinearGradient(
           colors: [
@@ -16,11 +16,28 @@ class HomeWidget extends StatelessWidget {
             Color(0xff6c72cb)
           ],
         ),
-        borderRadius: BorderRadius.all(Radius.circular(8)),
+          borderRadius: BorderRadius.all(Radius.circular(14))
       ),
-      child: Center(
-        child: Image.asset(
-          "lib/images/placement.png",
+      child: Container(
+        padding: const EdgeInsets.all(4),
+        decoration: const BoxDecoration(
+            color: Color(0xff17181f),
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+        ),
+        child: Column(
+          children: [
+            Center(
+              child: Image.asset(
+                image,
+              ),
+            ),
+            Text(title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ],
         ),
       ),
     );
